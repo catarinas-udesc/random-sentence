5d4622295f083672ce288926d06cd39e672db8d1
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2019-10-25 19:59:08 -0300

    Update frases.py

--- frases.py
+++ frases.py
@@ -2,4 +2,5 @@ frases = [
 "Ficar louco de vez em quando é necessidade básica para permanecer são. (Osho)", #Nome
 "Uma vez ao ano é lícito fazer loucuras. (Santo Agostinho)", #Nome
 "All that we are is the result of what we have thought. (Buddha)", #Luísa Kinas
+"Apesar de tudo eu ainda creio na bondade humana. (Anne Frank)" #Luciano Abreu
 ]
