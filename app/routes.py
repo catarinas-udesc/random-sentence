43805ede58e0fb449328757d52dea3dc20da5e15
Vinicius Gasparini <viniciuszeiko@gmail.com>
2019-10-25 20:52:58 -0300

    front end top

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,8 @@
 from app import app
 from flask import request, render_template, redirect, url_for
 from frases import frases
-import random
+import random as r
 
 @app.route('/')
 def index():
-    return render_template('index.html', frase=random.choice(frases))
+    return render_template('index.html', frase=r.choice(frases), cor="{},{},{}".format(r.randrange(255), r.randrange(255), r.randrange(255)))
