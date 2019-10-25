a5a59154978f68c82e8fe1fd7735aa5397f11073
Igor S. Froehner <48810009+IgorFroehner@users.noreply.github.com>
2019-10-25 19:52:46 -0300

    Frase adicionada

--- frases.py
+++ frases.py
@@ -1,4 +1,5 @@
 frases = [
 "Ficar louco de vez em quando é necessidade básica para permanecer são. (Osho)", #Nome
 "Uma vez ao ano é lícito fazer loucuras. (Santo Agostinho)", #Nome
+"Nos chamam de loucos, num mundo em que os certos fazem bombas.", #Igor Froehner
 ]
