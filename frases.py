bedeb99a09435282a4bf2ef46f628a2cd5baf953
Hakuyare <55406424+Hakuyare@users.noreply.github.com>
2019-10-25 19:55:55 -0300

    frase adicionada

--- frases.py
+++ frases.py
@@ -2,4 +2,5 @@ frases = [
 "Ficar louco de vez em quando é necessidade básica para permanecer são. (Osho)", #Nome
 "Uma vez ao ano é lícito fazer loucuras. (Santo Agostinho)", #Nome
 "All that we are is the result of what we have thought. (Buddha)", #Luísa Kinas
+"Eu prefiro morrer do que perder a vida!(Chaves)",#Vitor Bernstorff Clemes
 ]
