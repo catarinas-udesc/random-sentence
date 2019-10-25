9749fb01670333e40aae27f3c576517089af9282
Lais <lais@vvossen.com>
2019-10-25 20:09:16 -0300

    adicionando frase

--- frases.py
+++ frases.py
@@ -2,4 +2,5 @@ frases = [
 "Ficar louco de vez em quando é necessidade básica para permanecer são. (Osho)", #Nome
 "Uma vez ao ano é lícito fazer loucuras. (Santo Agostinho)", #Nome
 "All that we are is the result of what we have thought. (Buddha)", #Luísa Kinas
+"É um erro acreditar que é possível resolver qualquer problema importante usando batatas. (Douglas Adams)",
 ]
