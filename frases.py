d08665a013f1b0f6d4d1cb11bc3e770d29d97775
Jhey <57020774+JheyBerry@users.noreply.github.com>
2019-10-25 19:50:11 -0300

    Update frases.py
    Adicionei uma frase de Einstein

--- frases.py
+++ frases.py
@@ -1,5 +1,5 @@
 frases = [
 "Ficar louco de vez em quando é necessidade básica para permanecer são. (Osho)", #Nome
 "Uma vez ao ano é lícito fazer loucuras. (Santo Agostinho)", #Nome
-"All that we are is the result of what we have thought. (Buddha)", #Luísa Kinas
+"Uma pessoa que nunca cometeu um erro nunca tentou algo de novo. (ALbert Einstein)", #Jheney Vilvok
 ]
