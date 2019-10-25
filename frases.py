4d43ed3b7813d0b23ebf608730606a2bc8c27cee
Machado <elitonmachadod200@gmail.com>
2019-10-25 20:29:10 -0300

    New Frase

--- frases.py
+++ frases.py
@@ -2,4 +2,5 @@ frases = [
 "Ficar louco de vez em quando é necessidade básica para permanecer são. (Osho)", #Nome
 "Uma vez ao ano é lícito fazer loucuras. (Santo Agostinho)", #Nome
 "All that we are is the result of what we have thought. (Buddha)", #Luísa Kinas
+"O sucesso é um mestre terrível. Convence às pessoas inteligentes a pensar que não vão perder. (Bill Gates)" #Eliton Machado da Silva
 ]
