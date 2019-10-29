c0044da6e65a89543146b246e07662de51cc2e99
Eduarda Cristina <eduarda.crsa@gmail.com>
2019-10-29 09:20:21 -0300

    Some corrections

--- frases.py
+++ frases.py
@@ -5,11 +5,11 @@ frases = [
 "Nos chamam de loucos, num mundo em que os certos fazem bombas.", #Igor Froehner
 "All that we are is the result of what we have thought. (Buddha)", #Luísa Kinas
 "Eu prefiro morrer do que perder a vida!(Chaves)",#Vitor Bernstorff Clemes
-"É um erro acreditar que é possível resolver qualquer problema importante usando batatas. (Douglas Adams)" #Lais,
-"O sucesso é um mestre terrível. Convence às pessoas inteligentes a pensar que não vão perder. (Bill Gates)" #Eliton Machado da Silva
-"Apesar de tudo eu ainda creio na bondade humana. (Anne Frank)" #Luciano Abreu
+"É um erro acreditar que é possível resolver qualquer problema importante usando batatas. (Douglas Adams)", #Lais,
+"O sucesso é um mestre terrível. Convence às pessoas inteligentes a pensar que não vão perder. (Bill Gates)", #Eliton Machado da Silva
+"Apesar de tudo eu ainda creio na bondade humana. (Anne Frank)", #Luciano Abreu
 "Prefiro uma pedra no meu caminho que uma pedra nos meus rins", #Jaasiel Abner
-"Babyyyyy Shark"# Rafael Pereira
+"Babyyyyy Shark", # Rafael Pereira
 "Gentílico de quem nasce em Tubarão-SC é Baby Shark.", #MuriloLoffi
 "Simplicity is prerequisite for reliability.", #Vinicius Gasparini
 "Baby Shark tuturu", #Murilo Oliveira
@@ -17,8 +17,8 @@ frases = [
 "Babyyyyy Shark",# Rafael Pereira
 "Se tem uma coisa que acaba com meu dia, é a noite",#Natália Speck  
 "A vida e feita de escolhas, e tem gente que nao sabe escolher... (GRANZA, Rafael)",# Danilo Steps
-"Quando vires um homem bom, tenta imitá-lo; quando vires um homem mau, examina-te a ti mesmo. (Confúcio)," #None
-"Babuinos bobócas balbuciando em bando" #Eduarda
-"Jamais, em hipótese alguma, deixe um Vogon ler poesias para você. (O guia do mochileiro das galáxias)" #Eduarda
+"Quando vires um homem bom, tenta imitá-lo; quando vires um homem mau, examina-te a ti mesmo. (Confúcio)", #None
+"Babuinos bobócas balbuciando em bando", #Eduarda
+"Jamais, em hipótese alguma, deixe um Vogon ler poesias para você. (O guia do mochileiro das galáxias)", #Eduarda
 ]
 
